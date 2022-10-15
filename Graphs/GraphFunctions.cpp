69618fb409614a55b8f8fbeb1c141af2d65f2753
gh4ahbi <gh4abhi@gmail.com>
2022-10-15 06:27:35 +0530

    Added one function.

--- Graphs/GraphFunctions.cpp
+++ Graphs/GraphFunctions.cpp
@@ -1126,6 +1126,50 @@ vector<vector<int>> findBridges(int n, vector<vector<int>>& edges) {
         return ans;
     }
 
+//--------------------------Articulation Point in Graph------------------------------------
+
+    void DFS(ll src, ll par, ll timer, vector<ll> &tin, vector<ll> &low, vector<ll> &vis, vector<ll> &ans, vector<vector<ll>> adj)
+    {
+        vis[src] =1;
+        ll child = 0;
+        tin[src] = low[src] = timer++;
+        for(auto i: adj[src])
+        {
+            if(i==par)
+                continue;
+            if(vis[i]==0)
+            {
+                child++;
+                DFS(i,src,timer,tin,low,vis,ans,adj);
+                low[src] = min(low[src],low[i]);
+                if(tin[src]<=low[i] and par!=-1)
+                    ans[src] = 1;
+            }
+            else
+                low[src] = min(low[src],low[i]);
+        }
+        if(par==-1 and child>1)
+            ans[src] = 1;
+    }
+  
+    vector<int> articulationPoints(int V, vector<vector<ll>> adj) {
+        vector<ll> tin(V,-1);
+        vector<ll> low(V,-1);
+        vector<ll> vis(V,0);
+        vector<ll> ans(V,0);
+        ll timer = 0;
+        for(ll i=0; i<V;i++)
+            if(vis[i]==0)
+                DFS(i,-1,timer,tin,low,vis,ans,adj);
+        vector<ll> artPoint;
+        for(ll i=0;i<V;i++)
+            if(ans[i])
+                artPoint.push_back(i);
+        if(artPoint.size()==0)
+            artPoint.push_back(-1);
+        return artPoint;
+    }
+
 //--------------------------------------------------------------Main Function----------------------------------------------------------------------------------------------
 int main()
 {
