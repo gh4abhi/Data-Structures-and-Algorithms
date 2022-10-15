cfa2c6702b4d0de0f31ce774e673189d8edfe0da
gh4ahbi <gh4abhi@gmail.com>
2022-10-15 06:25:55 +0530

    Added one function.

--- Graphs/GraphFunctions.cpp
+++ Graphs/GraphFunctions.cpp
@@ -1089,6 +1089,42 @@ int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
     return dis[dest];
 }
 
+//--------------------------Bridges in Graph------------------------------------
+
+void DFSForBridges(ll src, ll par, vector<ll> &tin, vector<ll> &low, vector<ll> &vis, vector<vector<ll>> &adj, vector<vector<ll>> &ans, ll timer)
+    {
+        vis[src] = 1;
+        tin[src] = low[src] = timer++;
+        for(auto i:adj[src])
+        {
+            if(i==par)
+                continue;
+            if(vis[i]==0)
+            {
+                DFSForBridges(i,src,tin,low,vis,adj,ans,timer);
+                low[src] = min(low[src],low[i]);
+                if(tin[src]<low[i])
+                    ans.push_back({i,src});
+            }
+            else
+                low[src] = min(low[src],tin[i]);
+        }
+    }
+    
+vector<vector<int>> findBridges(int n, vector<vector<int>>& edges) {
+        vector<ll> tin(n,-1);
+        vector<ll> low(n,-1);
+        vector<ll> vis(n,0);
+        vector<vector<ll>> adj(n);
+        vector<vector<ll>> ans;
+        ll timer = 0;
+        for(auto i:edges)
+            adj[i[0]].push_back(i[1]), adj[i[1]].push_back(i[0]);
+        for(ll i=0;i<n;i++)
+            if(vis[i]==0)
+                DFSForBridges(i,-1,tin,low,vis,adj,ans,timer);
+        return ans;
+    }
 
 //--------------------------------------------------------------Main Function----------------------------------------------------------------------------------------------
 int main()
